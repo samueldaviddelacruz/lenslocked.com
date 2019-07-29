42941ae81426a34be654c7902baf203270456c1f
Samuel <alphaelena@gmail.com>
2019-07-29 12:41:48 -0400

    Created the users controllers and updated main.go to use it instead of signup func

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/samueldaviddelacruz/lenslocked.com/controllers"
 	"github.com/samueldaviddelacruz/lenslocked.com/views"
 )
 
 var (
 	homeView    *views.View
 	contactView *views.View
-	signUpView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -23,19 +23,16 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 
 }
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signUpView.Render(w, nil))
-}
 
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	signUpView = views.NewView("bootstrap", "views/signup.gohtml")
+	usersC := controllers.NewUsers()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New)
 	http.ListenAndServe(":4000", r)
 }
 
