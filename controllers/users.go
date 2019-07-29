4b900b0e511e80e1477eac1f4b7c27254ab8e647
Samuel <alphaelena@gmail.com>
2019-07-29 14:19:15 -0400

    started parsing the signup form

--- controllers/users.go
+++ controllers/users.go
@@ -17,6 +17,7 @@ func NewUsers() *Users {
 	}
 }
 
+// Users Represents a Users controller
 type Users struct {
 	NewView *views.View
 }
@@ -36,5 +37,11 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "This is a temp response")
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintln(w, r.PostForm["email"])
+	fmt.Fprintln(w, r.PostForm["password"])
+
 }
