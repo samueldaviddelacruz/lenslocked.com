6ddb652e1fabd89ea7b2a954a9906d29c481a9b9
Samuel <alphaelena@gmail.com>
2019-07-30 11:54:20 -0400

    Made it easier to create views by not passing in the views/ prefix and by not requiring the gohtml extension

--- views/view.go
+++ views/view.go
@@ -7,14 +7,19 @@ import (
 )
 
 var (
+
 	// LayoutDir holds the layouts root directory
 	LayoutDir = "views/layouts/"
 	// TemplateExt represents templates extension found on layout directory
 	TemplateExt = ".gohtml"
+	TemplateDir = "views/"
 )
 
 // NewView creates a new View
 func NewView(layout string, files ...string) *View {
+	addTemplatePath(files)
+	addTemplateExt(files)
+
 	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -54,4 +59,28 @@ func layoutFiles() []string {
 		panic(err)
 	}
 	return files
+}
+
+// addTemplatePath takes in a slice of strings
+// representing file paths for templates, and it prepends
+// the TemplateDir directory to each string in the slice
+//
+// Eg the input {"home"} would result in the output
+// {"views/home"} if TemplateDir == "views/"
+func addTemplatePath(files []string) {
+	for i, f := range files {
+		files[i] = TemplateDir + f
+	}
+}
+
+// addTemplateExt takes in a slice of strings
+// representing file paths for templates, and it appends
+// the TemplateExt extension to each string in the slice
+//
+// Eg the input {"home"} would result in the output
+// {"home.gohtml"} if TemplateExt == ".gohtml"
+func addTemplateExt(files []string) {
+	for i, f := range files {
+		files[i] = f + TemplateExt
+	}
 }
