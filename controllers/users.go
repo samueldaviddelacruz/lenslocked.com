cd3e2c6d00f07081990c0a1bae6f85f731961ac8
Samuel <alphaelena@gmail.com>
2019-08-02 11:07:22 -0400

    Connected our users controller to the users model

--- controllers/users.go
+++ controllers/users.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/samueldaviddelacruz/lenslocked.com/models"
+
 	"github.com/samueldaviddelacruz/lenslocked.com/views"
 )
 
 type SignupForm struct {
+	Name     string `schema:"name"`
 	Email    string `schema:"email"`
 	Password string `schema:"password"`
 }
@@ -16,15 +19,17 @@ type SignupForm struct {
 // This function will panic if the templates are not
 // parsed correctly, and should only be used during
 // initial setup
-func NewUsers() *Users {
+func NewUsers(us *models.UserService) *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "users/new"),
+		us:      us,
 	}
 }
 
 // Users Represents a Users controller
 type Users struct {
 	NewView *views.View
+	us      *models.UserService
 }
 
 // New is used to render the form where a user can
@@ -45,6 +50,14 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var form SignupForm
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
+	}
+	user := models.User{
+		Name:  form.Name,
+		Email: form.Email,
+	}
+	if err := u.us.Create(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	fmt.Fprintln(w, form)
--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/samueldaviddelacruz/lenslocked.com/controllers"
+	"github.com/samueldaviddelacruz/lenslocked.com/models"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "lenslocked_dev"
 )
 
 func main() {
+	psqlinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	us, err := models.NewUserService(psqlinfo)
+	must(err)
+	defer us.Close()
+	must(us.AutoMigrate())
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers()
+	usersC := controllers.NewUsers(us)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
@@ -17,6 +33,7 @@ func main() {
 
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+	fmt.Println("Starting the server on port :4000")
 	http.ListenAndServe(":4000", r)
 }
 
