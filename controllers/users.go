f933685c166ead4120dfd451b74e65985296e4b0
Samuel <alphaelena@gmail.com>
2019-08-08 09:44:27 -0400

    Implemented error handling for render errors

--- controllers/users.go
+++ controllers/users.go
@@ -42,9 +42,7 @@ type Users struct {
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 
-	if err := u.NewView.Render(w, nil); err != nil {
-		panic(err)
-	}
+	u.NewView.Render(w, nil)
 }
 
 // Create is used to process the signup form when a user
